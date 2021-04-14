bccdfbb07bde97728b803b01fa85eb2d614161dd
ndominica <dominika.nagy@femtonics.eu>
2021-04-14 16:34:55 +0000

    app.py created

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+data = pd.read_csv('data/csv')
+
+def convert(x):
+    year, week = x.split('-')
+    year = (int(year) - 2020) * 54
+    return year + int(week)
+
+data['week'] = data.year_week.apply(lambda x:convert(x))
+hun = data[data.country == 'Hungary']
+fig = px.line(data_frame=hun, x ='week', y='cumulative_count', color='indicator')
+
+
+st.title("Hello World!")
+st.write("Again!")
