0c23ccc3b7c844c7b45cfc562696eff15978ce1a
ndominica <dominika.nagy@femtonics.eu>
2021-06-29 23:22:47 +0200

    Python class practice homework

---
+++ covidProjekt/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='covidtools',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['covidtools'],
+zip_safe=False)
