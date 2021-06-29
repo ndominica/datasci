9b0399675b0c10ba5d49ec9cd8d5de3dbc8ffc9b
ndominica <dominika.nagy@femtonics.eu>
2021-06-29 15:29:09 +0200

    Assignment modifications, dockerfiles still scks

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.8.5
+
+WORKDIR /app
+
+COPY requirements.txt ./requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD streamlit run app.py
--- app.py
+++ app.py
@@ -3,19 +3,28 @@ import pandas as pd
 import plotly.express as px
 
 
-data = pd.read_csv('data/csv')
+
 
 def convert(x):
     year, week = x.split('-')
     year = (int(year) - 2020) * 54
     return year + int(week)
 
+
+# Data
+data = pd.read_csv('data/csv')
 data['week'] = data.year_week.apply(lambda x:convert(x))
-hun = data[data.country == 'Hungary']
-fig = px.line(data_frame=hun, x ='week', y='cumulative_count', color='indicator')
+list_of_countries = list(set(data['country']))
+list_of_countries.sort(reverse=False)
 
 
-st.title("Hello World!")
-country = st.selectbox('Select a country',['Hungary', 'Germany', 'Austria'])
+# Gui
+st.title("Worldwide Covid-19 Trends")
+country = st.selectbox('Select a country', list_of_countries)
 st.write('The selected country is {}'.format(country))
+
+
+# Gui filled with data
+country_data = data[data.country == country]
+fig = px.line(data_frame=country_data, x ='week', y='cumulative_count', color='indicator')
 st.plotly_chart(fig)
